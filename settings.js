ac4f410854a8af863a8cd2197dcd1bf670aa2ea4
Laxxy <118913811+LaxxyBot@users.noreply.github.com>
2022-11-25 00:04:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.APIKeys = {
 global.autoread = false // auto read message
 global.autobio = false 
 global.userRegister = false 
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "DimasBotz" //namabot kalian
-global.ownername= "𝐃𝐈𝐌𝐀𝐒 𝐓𝐎𝐊𝐈𝐒𝐀𝐊𝐈" //nama kalian
+global.botname = "LaxxyBotz" //namabot kalian
+global.ownername= "Laxxy Botz" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g" //bebas asal jan hapus
 global.myweb2 = "https://instagram.com/leviatnansyah?igshid=YmMyMTA2M2Y=" //bebas
-global.email = "leviadnansyahlevi@gmail.com" //bebas
+global.email = "iceostore442@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6283169648444'] //premium user
-global.owner = ["6283169648444","6283169648444"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6283169648444" // nomor wa kalian
-global.ownernomerr = "6283169648444" //nmr wa kalian
+global.premium = ['6281617967693'] //premium user
+global.owner = ["62881617967693","6281617967693"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281617967693" // nomor wa kalian
+global.ownernomerr = "6281617967693" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© DimasBotz' //sticker wm ubah
-global.author = 'Di Buat Oleh 𝐃𝐈𝐌𝐀𝐒 𝐓𝐎𝐊𝐈𝐒𝐀𝐊𝐈' //sticker wm ganti nama kalian
+global.packname = '© Laxxy Botz' //sticker wm ubah
+global.author = 'Di Buat Oleh Laxxy Botz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
